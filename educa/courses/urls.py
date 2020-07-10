1411a772ece7a23e7508a143cb585789ee3ea428
Duke09 <debopriyo09@outlook.com>
2020-07-10 08:15:43 +0530

    Templtes and url patterns, course module content CRUD view

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -28,4 +28,24 @@ urlpatterns = [
         CourseModuleUpdateView.as_view(),
         name='course_module_update'
     ),
+    path(
+        'module/<int:module_id>/content/<model_name>/create/',
+        ContentCreateUpdateView.as_view(),
+        name='module_content_create'
+    ),
+    path(
+        'module/<int:module_id>/content/<model_name>/<id>/',
+        ContentCreateUpdateView.as_view(),
+        name='module_content_update'
+    ),
+    path(
+        'content/<int:id>/delete/',
+        ContentDeleteView.as_view(),
+        name='module_content_delete'
+    ),
+    path(
+        'module/<int:module_id>/',
+        ModuleContentListView.as_view(),
+        name='module_content_list'
+    ),
 ]
