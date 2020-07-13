a77634948a1b68ec3a44d1639c93c5b00b894220
Duke09 <debopriyo09@outlook.com>
2020-07-13 10:23:22 +0530

    Custome api and authentication with rest

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ..models import Subject, Course, Module
+from ..models import Subject, Course, Module, Content
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,6 +16,38 @@ class ModuleSerializer(serializers.ModelSerializer):
     
 class CourseSerializer(serializers.ModelSerializer):
     modules = ModuleSerializer(many=True, read_only=True)
+    class Meta:
+        model = Course
+        fields = [
+            'id', 'subject', 'title', 'slug',
+            'overview', 'created', 'owner',
+            'modules'
+        ]
+
+class ItemRealatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRealatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+class ModuleWithContentSetializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = [
+            'order', 'title', 
+            'description', 'contents'
+        ]
+
+class CourseWithContentSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentSetializer(many=True)
+
     class Meta:
         model = Course
         fields = [
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -7,7 +7,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 
 from ..models import Subject, Course
-from .serializers import SubjectSerializer, CourseSerializer
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentSerializer
+from .permissions import IsEnrolled
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -38,3 +39,10 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True, methods=['get'],
+            serializer_class=CourseWithContentSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
